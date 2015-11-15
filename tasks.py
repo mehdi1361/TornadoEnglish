218fe0698fd6116fbda82fbbcc04c4fbc4595dc2
mehdi1361 <mhd.mosavi@gmail.com>
2015-11-15 16:01:07 +0330

    bg fix:unicode add to db

--- tasks.py
+++ tasks.py
@@ -4,20 +4,23 @@ import MySQLdb
 import datetime
 import collections
 import json
+
 app = Celery('tasks', broker='amqp://guest@localhost//')
 
 # @app.task(trail=True)
 def sign_up(email, username, password):
-    db = MySQLdb.connect("127.0.0.1", "root", "13610522", "Learning_English")
+    db = MySQLdb.connect("127.0.0.1", "root", "13610522", "Learning_English", charset='utf8')
     cursor = db.cursor()
-    result = cursor.execute('''INSERT INTO users(email, username, password) VALUES (%s, %s, %s);''', (email, username, password))
+    result = cursor.execute('''INSERT INTO users(email, username, password) VALUES (%s, %s, %s);''',
+                            (email, username, password))
     write_to_file.delay(email, result)
     db.commit()
     db.close()
 
+
 # @app.task()
 def sign_in(email, password):
-    db = MySQLdb.connect("localhost", "root", "13610522", "Learning_English")
+    db = MySQLdb.connect("localhost", "root", "13610522", "Learning_English", charset='utf8')
     cursor = db.cursor()
     cursor.execute('''select email,password,user_id,username,image_url,score from users
                                where email = '%s' and password=%s limit 1;''' % (email, password))
@@ -37,17 +40,20 @@ def sign_in(email, password):
     j = json.dumps(objects_list)
     return j
 
+
 # @app.task()
 def db_set_level(level_title, level_description):
-    db = MySQLdb.connect("127.0.0.1", "root", "13610522", "Learning_English")
+    db = MySQLdb.connect("127.0.0.1", "root", "1361522", "Learning_English", charset='utf8')
     cursor = db.cursor()
-    result = cursor.execute('''INSERT INTO level(level_title, level_description) VALUES ('%s', '%s');'''% (level_title, level_description))
+    result = cursor.execute(
+        '''INSERT INTO level(level_title, level_description) VALUES ('%s', '%s');''' % (level_title, level_description))
     write_to_file.delay(level_title, result)
     db.commit()
     db.close()
 
+
 def db_get_level():
-    db = MySQLdb.connect("localhost", "root", "13610522", "Learning_English")
+    db = MySQLdb.connect("localhost", "root", "13610522", "Learning_English", charset='utf8')
     cursor = db.cursor()
     cursor.execute('''select level_id,level_title,level_description from level;''')
     db.commit()
@@ -63,10 +69,21 @@ def db_get_level():
     j = json.dumps(objects_list)
     return j
 
-def get_lesson(level_id):
+
+def db_set_lesson(level_title, level_description):
+    db = MySQLdb.connect("127.0.0.1", "root", "1361522", "Learning_English", charset='utf8')
+    cursor = db.cursor()
+    result = cursor.execute( '''INSERT INTO lesson(level_title, level_description) VALUES ('%s', '%s');''' % (level_title, level_description))
+    write_to_file.delay(level_title, result)
+    db.commit()
+    db.close()
+
+
+def db_get_lesson(level_id):
     db = MySQLdb.connect("localhost", "root", "1361522", "Learning_English")
     cursor = db.cursor()
-    cursor.execute('''select l.lesson_id,l.lesson_title,l.lesson_description from lesson l where l.level_level_id=%s;'''% level_id)
+    cursor.execute(
+        '''select l.lesson_id,l.lesson_title,l.lesson_description from lesson l where l.level_level_id=%s;''' % level_id)
     db.commit()
     db.close()
     rows = cursor.fetchall()
@@ -79,6 +96,8 @@ def get_lesson(level_id):
         objects_list.append(d)
     j = json.dumps(objects_list)
     return j
+
+
 @app.task
 def write_to_file(email, Result):
     print "Hello"
@@ -86,4 +105,3 @@ def write_to_file(email, Result):
     f.write('insert to DataBase with email: %s Successfull:{%s} at :%s\n'
             % (email, Result, datetime.datetime.now()))
     f.close()
-
